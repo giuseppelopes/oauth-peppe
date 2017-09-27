236117ae2340d85f6d9a841e8310da4cdd3e9f49
Giuseppe Lopes <giuseppe.lopes@gmail.com>
2017-09-26 23:22:14 -0300

    Changed Application

--- src/main/java/auth/Application.java
+++ src/main/java/auth/Application.java
@@ -1,10 +1,9 @@
-package auth.api;
+package auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
